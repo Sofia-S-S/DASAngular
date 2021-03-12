05279941d22cc0579ec3e021de8f0b111c7149df
Sofia-S <sofka.vs@gmail.com>
2021-03-12 00:32:17 -0600

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { DoctorComponent } from './components/doctor/doctor.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //A route is an object with two properties
+  {
+    path: "",
+    component: LoginComponent,
+  },
+  {
+    path: "admin",
+    component: AdminComponent,
+  },
+  {
+    path: "doctor",
+    component: DoctorComponent,
+  },
+  {
+    path: "patient",
+    component: AdminComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { DoctorComponent } from './components/doctor/doctor.component';
+import { PatientComponent } from './components/patient/patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    AdminComponent,
+    DoctorComponent,
+    PatientComponent
   ],
   imports: [
     BrowserModule,
