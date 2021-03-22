fc1793b0c390513100e8968226aeb26f954e7ffb
Sofia-S <sofka.vs@gmail.com>
2021-03-21 23:34:45 -0500

    admin complited

--- src/app/service/admin.service.ts
+++ src/app/service/admin.service.ts
@@ -15,9 +15,17 @@ export class AdminService {
   getAllDoctors():Observable<User[]>{
     return this.httpClient.get("http://localhost:8080/user/all") as Observable<User[]>
   }
+  getDoctorById(userId :number):Observable<User>{
+    
+    return this.httpClient.get("http://localhost:8080/user/id?userId="+userId) as Observable<User>
+  }
 
   createDoctor(doctor:User):Observable<User>{
     return this.httpClient.post<User>("http://localhost:8080/admin/new-doctor", doctor) 
+  }
+
+  updateDoctor(doctor:User):Observable<User>{
+    return this.httpClient.post<User>("http://localhost:8080/admin/update-doctor", doctor) 
   }
 
   getAllBills():Observable<Bill[]>{
